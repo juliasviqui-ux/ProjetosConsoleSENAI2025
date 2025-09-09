728fba92a80f284bebe8a064974f2f72e3be19c4
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 09:20:27 -0300

    Atividade 4 - Idade em dias

---
+++ atv 4/atv 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite sua idade em anos: ");
+int anos = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite os meses: ");
+int meses = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite os dias: ");
+int dias = Convert.ToInt32(Console.ReadLine());
+int idadeEmDias= (anos * 365) + (meses * 30) + dias;
+Console.WriteLine("Sua idade em dias é: " + idadeEmDias);
