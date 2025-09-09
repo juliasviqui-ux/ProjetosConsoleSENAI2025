617c1c26371d9a8df970acc867e2d9a5d7a15bc7
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 10:40:27 -0300

    Atividade 10 - Média das notas

---
+++ atv 10/atv 10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite a nota da primeria avaliação: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a nota da segunda valiação: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+double media = (nota1 + nota2) / 2;
+Console.WriteLine("\nMédia do aluno: " + media.ToString("F2"));
+if (media >= 6)
+{
+    Console.WriteLine("Aluno aprovado!");
+}
+else
+{
+    Console.WriteLine("Aluno Reprovado!");
+}
