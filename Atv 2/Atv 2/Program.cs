cffb9e9bb6c716e6c7ba26e9a5a5f8904e4d4c34
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 09:05:49 -0300

    Atividade 2 - Antecessor

---
+++ Atv 2/Atv 2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Escreva um valor e vamos descobrir o antecessor dele!");
+int valor = int.Parse(Console.ReadLine()!);
+int antecessor = valor -1;
+Console.WriteLine($"Agora, o antecessor do número é igual a {antecessor}");
