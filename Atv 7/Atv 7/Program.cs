688bedb11d54e6b4e56cf921b84ce3178924ed73
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 08:13:22 -0300

    Exercício 7 - Valor < ou > que 10

---
+++ Atv 7/Atv 7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Escreva um valor para que eu saiba se ele é maior ou menor que 10");
+int valor = int.Parse(Console.ReadLine()!); 
+if (valor < 10 )
+{
+    Console.WriteLine("Não é maior que 10!");
+}
+else if ( (valor > 10 )
+{
+    Console.WriteLine("O valor é maior que 10!");
+}
+else (valor = 10)
+{
+    Console.WriteLine("O valor é igual a 10!");
+}
