4a87e38c923e2489d51e49f4e641a0d876eb1d54
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 10:32:04 -0300

    Atividade 9 - Compra das maçãs

---
+++ atv 9/atv 9/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Escreva a quantidade maçãs compradas: ");
+int quantidade = int.Parse(Console.ReadLine());
+if (quantidade < 12)
+{
+    Console.WriteLine("O valor unitário de cada maçã vai ser R$1,30)");
+}
+else
+{
+    Console.Write("O valor da maçã vai ser R$1,00");
+}
