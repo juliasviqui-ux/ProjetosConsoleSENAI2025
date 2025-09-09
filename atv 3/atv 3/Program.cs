803c5f912390f10c46ad4726366195d729536cdd
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 09:13:30 -0300

    Atividade 3 - Base X Altura do Retângulo

---
+++ atv 3/atv 3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite a base do retângulo.");
+double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a altura do retângulo.");
+double alturaRetangulo  = Convert.ToDouble(Console.ReadLine()); 
+double area = baseRetangulo * alturaRetangulo;
+Console.WriteLine("A área do retângulo é: " + area);
