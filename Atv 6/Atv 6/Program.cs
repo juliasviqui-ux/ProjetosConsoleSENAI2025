8a6ac90a63bb9307f5be64b0171aaa9b8b2a697f
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 10:20:26 -0300

    Atvidade 6 - Reajuste no salário

---
+++ Atv 6/Atv 6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Digite o salário atual do funcionário : R$ ");
+double SalarioAtual = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o percentual de reajuste (%): ");
+double percentualReajuste  = Convert.ToDouble(Console.ReadLine());
+double NovoSalario = SalarioAtual + (SalarioAtual * percentualReajuste / 100);
+Console.WriteLine("\nO novo salário é: R$ " +  NovoSalario.ToString("F2"));
