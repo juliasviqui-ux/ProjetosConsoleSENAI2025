dab6ca0928449e52246240de0276fdcb7eba94d0
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 08:41:14 -0300

    Atividade 8  - Valor positivo ou negativo!

--- Atv 8/Atv 8/Program.cs
+++ Atv 8/Atv 8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Escreva um valor e vamos ver se ele é positivo ou negativo");
+int valor = int.Parse(Console.ReadLine());
+if (valor > 0)
+{
+    Console.WriteLine("O valor é positivo!");
+}
+else 
+{
+    Console.WriteLine("O valor é negativo!");
+}
