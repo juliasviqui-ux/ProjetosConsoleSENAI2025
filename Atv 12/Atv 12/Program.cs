9c438cdd0d311720ec09dcb450875afb72a1415d
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 11:32:58 -0300

    Atividade 12 - Um número maior que o outro

---
+++ Atv 12/Atv 12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite o primeiro valor: ");
+double valor1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor: ");
+double valor2  = Convert.ToDouble(Console.ReadLine());
+if (valor1 > valor2)
+{
+    Console.WriteLine($"\nO maior valor é: {valor1}");
+}
+else
+{
+    Console.WriteLine($"\nO maior valor é: {valor2}");
+}
