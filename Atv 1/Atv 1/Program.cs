c677bbb8ead69420496662adcbab6495b5149e66
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 08:53:13 -0300

    Atividade 1 - Valor de A e B

---
+++ Atv 1/Atv 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Escreva o valor para A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Escreva o valor de B");
+int B = int.Parse(Console.ReadLine()!);
+int C;
+C = A;
+A = B;
+B = C;
+Console.WriteLine($"Agora, o valor de A = {A}, o valor de B = {B}");
