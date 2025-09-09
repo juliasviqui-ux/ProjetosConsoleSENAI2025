82b5a201a075107ba9063d7eb6f61fadfb2a74cf
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-09 11:54:05 -0300

    Atividade 13 - Valores em ordem crescente

--- Atv 13/Atv 13/Program.cs
+++ Atv 13/Atv 13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o primeiro valor: ");
+double valor1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o segundo valor: ");
+double valor2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o terceiro valor: ");
+double valor3 = Convert.ToDouble(Console.ReadLine());
+double menor, meio, maior;
+if (valor1 < valor2 && valor1 < valor3)
+    menor = valor1;
+else if (valor2 < valor1 && valor2 < valor3)
+    menor = valor2;
+else
+    menor = valor3;
+if (valor1 > valor2 && valor1 > valor3)
+    maior = valor1;
+else if (valor2 > valor1 && valor2 > valor3)
+    maior = valor2;
+else
+    maior = valor3;
+meio = valor1 + valor2 + valor3 - menor - maior;
+Console.WriteLine($"\nValores em ordem crescente: {menor}, {meio}, {maior}");
