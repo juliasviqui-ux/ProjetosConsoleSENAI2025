7219b197cb4c30807d43f151532c56fe812dd26f
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-17 08:34:17 -0300

    Valores interios de 1 até n.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um valor N: ");
+int N = int.Parse(Console.ReadLine());
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    Console.WriteLine("O valor de N deve ser meior que zero.");
+}
