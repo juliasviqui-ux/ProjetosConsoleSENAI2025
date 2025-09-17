42b529ea15892aff632b8908149a8f4dc4db9316
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-17 08:49:11 -0300

    Números entre 1.000 e 2.000.

---
+++ atv 15/atv 15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Números entre 1.000 e 2.000 que quando divididos por 11, deixam o resto 5: ");
+for (int i = 1000; i <= 2000; i++)
+{
+    if (i % 11 == 5) 
+    {
+        Console.WriteLine(i);
+    }
+}
