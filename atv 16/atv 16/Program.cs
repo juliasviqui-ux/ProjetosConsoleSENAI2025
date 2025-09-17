7c3ae1eeda9429b6ed7363b0b672677381329e3f
juliasviqui-ux <julia.sviqui@Gmail.com>
2025-09-17 09:32:46 -0300

    Número fatorial.

---
+++ atv 16/atv 16/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("digite um valor n: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("Fatorial de número negativo não existe.");
+}
+else if (n == 0)
+{
+    Console.WriteLine("0! = 1");
+}
+else
+{
+    long fatorial = 1; // uso long para suportar números maiores
+    for (int i = 1; i <= n; i++)
+    {
+        fatorial *= i; // multiplica o valor atual pelo contador
+    }
+    Console.WriteLine($"{n}! = {fatorial}");
+}
